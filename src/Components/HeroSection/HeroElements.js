ef7aaecbbbebb7cda75eb6c2fcc263c389aa5d6f
israelaniek <israelaniek16@gmail.com>
2021-09-28 18:33:17 -0500

    Mobile Responsive Hero Section

--- src/Components/HeroSection/HeroElements.js
+++ src/Components/HeroSection/HeroElements.js
@@ -5,7 +5,7 @@ import { Link as LinkS } from "react-scroll";
 export const HeroContainer = styled.div`
   width: 100%;
   height: 76vh;
-  padding: 50px 200px;
+  /* padding: 50px 200px; */
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -15,39 +15,58 @@ export const HeroContainer = styled.div`
       rgba(0, 0, 0, 0.75)
     ),
     url("https://daks2k3a4ib2z.cloudfront.net/57c2c646739f273554acc4af/57c2d86bb166809064181470_hero-bg.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
   color: #fff;
+
+  
 `;
 export const ContentWrapper = styled.div`
-  width: 1170px;
-  height: 65vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   margin-top: 50px;
   flex-direction: column;
   align-items: center;
 `;
 export const HeroHeading = styled.h1`
-  font-size: 80px;
+  font-size: 58px;
   letter-spacing: 2px;
-  margin-top: 100px;
-  margin-bottom: 30px;
+  text-align: center;
+  margin-top: 150px;
+  margin-bottom: 50px;
   font-weight: 100;
+
+  @media screen and (max-width: 996px) {
+    font-size: 36px;
+  }
 `;
 export const HeroParagraph = styled.p`
   text-align: center;
   font-size: 18px;
-  margin-bottom: 60px;
+  margin-bottom: 80px;
   letter-spacing: 1.2px;
-  line-height: 30px;
+  line-height: 28px;
+
+  @media screen and (max-width: 996px) {
+    font-size: 16px;
+    margin-bottom: 30px;
+  }
 `;
 export const HeroBtnWrapper = styled.div`
   display: flex;
 `;
 
-export const ArrowWrapper = styled(LinkS)``;
+export const ArrowWrapper = styled(LinkS)`
+  margin-top: 60px;
+
+  @media screen and (max-width: 996px) {
+    margin-top: 20px;
+  }
+`;
 
 export const AngleDown = styled(FaAngleDown)`
   font-size: 50px;
-  margin-top: 60px;
 
   &:hover {
     color: #4d8cf5;
